c32422f337cd1dc463bcf379c37a760fbfff0d81
Esteban <estebanguzra@gmail.com>
2019-10-26 07:02:18 -0600

    config

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ server.use(bodyParser.json());
 const neo4j = require("neo4j-driver").v1;
 
 const driver = neo4j.driver(
-  "bolt://localhost:7687",
-  neo4j.auth.basic("neo4j", "1234")
+  "bolt://10.104.133.37:7687",
+  neo4j.auth.basic("neo4j", "")
 );
 const session = driver.session();
 
